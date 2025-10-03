acceldata-io/git_sync

commit b91a1bb9f9ae1e1859a060ead05fa2a801f22f0f

ODP-5225: Enable syncing tags for fork branches without configured parents

src/github/match_args.rs
@@ -179,12 +179,11 @@ async fn match_tag_cmds(
     client: &GithubClient,
     repos: Vec<String>,
     cmd: &TagCommand,
-    _fork_workaround: HashMap<String, String>,
+    fork_workaround: HashMap<String, String>,
 ) -> Result<(), GitError> {
     let result = async {
         match cmd {
             TagCommand::Compare(compare_cmd) => {
-                //compare_cmd.validate().map_err(GitError::Other)?;
                 let repository = compare_cmd.repository.as_ref();
                 if compare_cmd.all && !repos.is_empty() {
                     client.diff_all_tags(repos).await?;
@@ -226,12 +225,19 @@ async fn match_tag_cmds(
                 let repository = sync_cmd.repository.as_ref();
                 // By default, this is false
                 let process_annotated_tags = sync_cmd.with_annotated;
+
                 if sync_cmd.all {
                     client
-                        .sync_all_tags(process_annotated_tags, &repos[..])
+                        .sync_all_tags(process_annotated_tags, &repos[..], fork_workaround)
                         .await?;
                 } else if let Some(repository) = repository {
-                    client.sync_tags(repository, process_annotated_tags).await?;
+                    if let Some(parent) = fork_workaround.get(repository) {
+                        client
+                            .sync_tags(repository, Some(parent), process_annotated_tags)
+                            .await?;
+                    } else {
+                        return Err(GitError::NoUpstreamRepo);
+                    }
                 } else {
                     return Err(GitError::MissingRepositoryName);
                 }

src/github/tag.rs
@@ -28,6 +28,7 @@ use octocrab::params::repos::Reference;
 use serde::Deserialize;
 use std::collections::{HashMap, HashSet};
 use std::error::Error;
+use std::hash::Hash;
 use std::process::Command;
 use temp_dir::TempDir;
 
@@ -318,10 +319,22 @@ impl GithubClient {
     pub async fn sync_tags<T: AsRef<str> + Display + Copy>(
         &self,
         url: T,
+        parent_url: Option<T>,
         process_annotated: bool,
     ) -> Result<(), GitError> {
         let info = get_repo_info_from_url(url)?;
-        let parent = self.get_parent_repo(url).await?;
+
+        let parent = if let Some(u) = parent_url {
+            let info = get_repo_info_from_url(u)?;
+            RepoInfo {
+                repo_name: info.repo_name,
+                owner: info.owner,
+                url: u.to_string(),
+                main_branch: info.main_branch,
+            }
+        } else {
+            self.get_parent_repo(url).await?
+        };
         let (owner, repo) = (info.owner, info.repo_name);
         let all_tags = self.compare_tags(url, &parent).await?;
 
@@ -442,15 +455,17 @@ impl GithubClient {
         Ok(())
     }
     /// Sync tags for all configured repositories
-    pub async fn sync_all_tags<T: AsRef<str> + ToString + Display>(
+    pub async fn sync_all_tags<T: AsRef<str> + ToString + Display + Eq + Hash>(
         &self,
         process_annotated: bool,
         repositories: &[T],
+        fork_workaround: HashMap<T, T>,
     ) -> Result<(), GitError> {
         let mut futures = FuturesUnordered::new();
         for url in repositories {
+            let parent_url = fork_workaround.get(url);
             futures.push(async move {
-                let result = self.sync_tags(url, process_annotated).await;
+                let result = self.sync_tags(url, parent_url, process_annotated).await;
                 (url, result)
             });
         }

commit d2b4993d4050ee6a51f582b6289c4c96381372ef

ODP-5225: Fix previously broken behaviour introduced in b91a1bb9f9ae1e1859a060ead05fa2a801f22f0f

src/github/match_args.rs
@@ -236,7 +236,7 @@ async fn match_tag_cmds(
                             .sync_tags(repository, Some(parent), process_annotated_tags)
                             .await?;
                     } else {
-                        return Err(GitError::NoUpstreamRepo);
+                        client.sync_tags(repository, None, process_annotated_tags).await?;
                     }
                 } else {
                     return Err(GitError::MissingRepositoryName);