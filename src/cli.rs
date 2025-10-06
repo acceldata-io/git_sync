acceldata-io/git_sync

commit 8ad706c969efb166a9d3092124e198647e29db0c

Added syncing/backups for forks that need to specify their parent repository

src/cli.rs
@@ -217,6 +217,11 @@ pub struct SyncTagCommand {
     /// read/write permissions for the repositories you are syncing.
     #[arg(short, long, default_value_t = false)]
     pub with_annotated: bool,
+
+    /// Sync tags in repositories that are forks, but do not have the parent repository set in a way that
+    /// Github understands.
+    #[arg(long, default_value_t = false, hide = true)]
+    pub with_fork_workaround: bool,
 }
 
 // --- Repo Commands ---
@@ -236,6 +241,7 @@ pub struct SyncTagCommand {
         ArgGroup::new("branch_conflict")
         .args(&["branch", "all"])
 ))]
+#[allow(clippy::struct_excessive_bools)]
 pub struct SyncRepoCommand {
     /// The repository to sync
     #[arg(short, long)]
@@ -253,6 +259,10 @@ pub struct SyncRepoCommand {
     /// Sync a specific branch. Not a valid option when using --all or --recursive
     #[arg(short, long)]
     pub branch: Option<String>,
+    /// Sync branches in repositories that are forks, but do not have the parent repository set in a way that
+    /// Github understands.
+    #[arg(long, default_value_t = false)]
+    pub with_fork_workaround: bool,
 }
 
 /// Check repositories for various conditions

src/config.rs
@@ -216,6 +216,14 @@ impl Config {
         });
         results
     }
+    /// Get all repository urls for the fork workaround group
+    fn get_fork_workaround_repositories(&self) -> Vec<String> {
+        if let Some(fork_workarounds) = &self.repos.fork_workaround {
+            fork_workarounds.keys().cloned().collect()
+        } else {
+            Vec::new()
+        }
+    }
     /// Get all repositories defined in the config file
     pub fn get_all_repositories(&self) -> Vec<String> {
         self.repos
@@ -226,6 +234,7 @@ impl Config {
             .chain(self.get_fork_repositories())
             .chain(self.get_private_repositories())
             .chain(self.get_all_custom_group_repositories())
+            .chain(self.get_fork_workaround_repositories())
             .collect()
     }
 }

src/github/match_args.rs
@@ -79,17 +79,22 @@ pub async fn match_arguments(app: &AppArgs, config: Config) -> Result<(), GitErr
         }
     };
     // Remove any duplicate repositories. This shouldn't have any meaningful performance impact
-    // since there won't every be thousands of repositories configured.
+    // since there won't ever be thousands of repositories configured.
+    // Collect it into a Vec so that we have a consistent ordered collection
     let repos = repos
         .into_iter()
         .collect::<HashSet<_>>()
         .into_iter()
         .collect::<Vec<_>>();
 
+    // If for some reason we cannot get the number of threads and the user doesn't try to define it,
+    // default to 4, which seems like a reasonable minimum expectation for most systems.
     let default_jobs = std::thread::available_parallelism()
         .map(std::num::NonZero::get)
         .unwrap_or(4);
-    let jobs: usize = app.jobs.unwrap_or(default_jobs);
+
+    // This value must be greater than 0
+    let jobs: usize = std::cmp::min(app.jobs.unwrap_or(default_jobs), 1);
     let client = GithubClient::new(&token, &config, jobs)?;
     if !token.is_empty() && verbose {
         let (rest_limit, graphql_limit) =
@@ -103,8 +108,10 @@ pub async fn match_arguments(app: &AppArgs, config: Config) -> Result<(), GitErr
         }
     }
     match &app.command {
-        Command::Tag { cmd } => match_tag_cmds(&client, repos, cmd).await?,
-        Command::Repo { cmd } => match_repo_cmds(&client, repos, config, cmd).await?,
+        Command::Tag { cmd } => match_tag_cmds(&client, repos, cmd, app.repository_type).await?,
+        Command::Repo { cmd } => {
+            match_repo_cmds(&client, repos, config, cmd, app.repository_type).await?
+        }
         Command::Branch { cmd } => match_branch_cmds(&client, repos, cmd, quiet).await?,
         Command::Release { cmd } => match_release_cmds(&client, repos, config, cmd).await?,
         Command::PR { cmd } => match_pr_cmds(&client, repos, config, cmd).await?,
@@ -157,6 +164,7 @@ async fn match_tag_cmds(
     client: &GithubClient,
     repos: Vec<String>,
     cmd: &TagCommand,
+    reposotory_type: RepositoryType,
 ) -> Result<(), GitError> {
     let result = async {
         match cmd {
@@ -279,16 +287,37 @@ async fn match_repo_cmds(
     repos: Vec<String>,
     config: Config,
     cmd: &RepoCommand,
+    repository_type: RepositoryType,
 ) -> Result<(), GitError> {
     match cmd {
         RepoCommand::Sync(sync_cmd) => {
             let repository = sync_cmd.repository.as_ref();
             let recursive = sync_cmd.recursive;
             let branch = sync_cmd.branch.as_ref();
+            let forks_with_workaround = config.repos.fork_workaround.clone().unwrap_or_default();
+            let process_forks_workaround = sync_cmd.with_fork_workaround;
             if sync_cmd.all {
-                client.sync_all_forks(&repos[..], recursive).await?;
+                if repository_type == RepositoryType::Fork && process_forks_workaround {
+                    let (task1, task2) = tokio::join!(
+                        client.sync_all_forks_workaround(forks_with_workaround),
+                        client.sync_all_forks(&repos[..], recursive),
+                    );
+                    task1?;
+                    task2?;
+                } else {
+                    client.sync_all_forks(&repos[..], recursive).await?;
+                }
             } else if let Some(repository) = repository {
-                if recursive {
+                // Process repositories using git
+                if let Some(parent) = forks_with_workaround.get(repository)
+                    && process_forks_workaround
+                {
+                    client.sync_with_upstream(repository, parent).await?;
+                } else if forks_with_workaround.contains_key(repository) {
+                    return Err(GitError::Other(format!(
+                        "Repository '{repository}' has no configured parent. Use the '--with-fork-workaround' flag to enable syncing it."
+                    )));
+                } else if recursive {
                     client.sync_fork_recursive(repository).await?;
                 } else {
                     client.sync_fork(repository, branch).await?;

Cargo.toml
@@ -3,7 +3,7 @@ authors = ["Jeffrey Smith <jeffrey.smith7@gmail.com>"]
 edition = "2024"
 license = "Apache-2.0"
 name    = "git_sync"
-version = "0.3.0"
+version = "0.4.0"
 
 
 [dependencies]

src/github/branch.rs
@@ -200,8 +200,10 @@ impl GithubClient {
         match res {
             Ok(_) => {
                 if !quiet {
-                    self.append_slack_message(format!("New branch {new_branch} created"))
-                        .await;
+                    self.append_slack_message(format!(
+                        "{owner}/{repo} - New branch {new_branch} created"
+                    ))
+                    .await;
                 }
                 Ok(())
             }

src/github/fork.rs
@@ -19,7 +19,7 @@ under the License.
 
 use crate::error::{GitError, is_retryable};
 use crate::github::client::GithubClient;
-use crate::utils::repo::get_repo_info_from_url;
+use crate::utils::repo::{get_repo_info_from_url, http_to_ssh_repo};
 use crate::{async_retry, handle_api_response};
 use chrono::DateTime;
 use futures::{StreamExt, stream::FuturesUnordered};
@@ -27,7 +27,9 @@ use octocrab::params::repos::Reference;
 use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, HashSet};
 use std::fmt::{Display, Write};
+use std::process::Command;
 use std::sync::Arc;
+use temp_dir::TempDir;
 
 /// Graphql query to fetch branches and their commit dates
 static GRAPHQL_QUERY: &str = r#"
@@ -286,6 +288,189 @@ impl GithubClient {
                 }
                 Err(e) => {
                     println!("❌ Failed to sync {repo}: {e}");
+                    errors.push((repo.to_string(), e));
+                }
+            }
+        }
+        if !errors.is_empty() {
+            return Err(GitError::MultipleErrors(errors));
+        }
+        Ok(())
+    }
+    /// This is for syncing repositories that have upstream repositories, but do not have them
+    /// configured. We have to do it manually instead of using the Github API. This only supports
+    /// fast forward merges, and will report errors for branches that cannot be fast-forwarded.
+    pub async fn sync_with_upstream<T: AsRef<str> + ToString + Display + Copy>(
+        &self,
+        url: T,
+        upstream: T,
+    ) -> Result<(), GitError> {
+        let ssh_url = http_to_ssh_repo(url)?;
+        let info = get_repo_info_from_url(url)?;
+        let last_part = info.repo_name;
+
+        let upstream = upstream.to_string();
+        let upstream_url = upstream.clone();
+        let _lock = Arc::clone(&self.semaphore).acquire_owned().await?;
+
+        let task: Result<(usize, usize), GitError> = tokio::task::spawn_blocking(move || {
+            let tmp_dir = TempDir::new()
+                .map_err(|e| GitError::Other(format!("Failed to create temp dir: {e}")))?;
+            let tmp = tmp_dir.path();
+            let tmp_str_base = tmp
+                .to_str()
+                .ok_or_else(|| GitError::Other("Temp dir not valid UTF-8".to_string()))?;
+
+            let tmp_str = format!("{tmp_str_base}/{last_part}");
+
+            Command::new("git")
+                .args(["-C", tmp_str_base, "clone", &ssh_url])
+                .status()?;
+
+            Command::new("git")
+                .args([
+                    "-C",
+                    tmp_str.as_str(),
+                    "remote",
+                    "add",
+                    "upstream",
+                    upstream.as_str(),
+                ])
+                .status()?;
+
+            Command::new("git")
+                .args(["-C", tmp_str.as_str(), "fetch", "upstream"])
+                .status()?;
+            let output = Command::new("git")
+                .args(["-C", tmp_str.as_str(), "branch", "-r"])
+                .output()?;
+
+            let branch_outputs = String::from_utf8_lossy(&output.stdout);
+
+            let origin: HashSet<String> = branch_outputs
+                .lines()
+                .filter(|line| line.trim().starts_with("origin/"))
+                .map(|line| line.trim().replace("origin/", ""))
+                .collect();
+
+            let upstream: HashSet<String> = branch_outputs
+                .lines()
+                .filter(|line| line.trim().starts_with("upstream/"))
+                .map(|line| line.trim().replace("upstream/", ""))
+                .collect();
+
+            let common_branches: Vec<_> = origin.intersection(&upstream).cloned().collect();
+
+            println!("Common: {common_branches:#?}");
+
+            let mut errors: Vec<GitError> = Vec::with_capacity(common_branches.len());
+            let mut successful: usize = 0;
+            let mut no_update = 0;
+
+            for branch in &common_branches {
+                println!("Checking out {branch} in {tmp_str}");
+                Command::new("git")
+                    .args([
+                        "-C",
+                        tmp_str.as_str(),
+                        "checkout",
+                        "--track",
+                        &format!("origin/{branch}"),
+                    ])
+                    .status()?;
+
+                let ff_merge = Command::new("git")
+                    .args([
+                        "-C",
+                        tmp_str.as_str(),
+                        "merge",
+                        "--ff-only",
+                        &format!("upstream/{branch}"),
+                    ])
+                    .output()?;
+                let status = ff_merge.status;
+                if status.success() {
+                    successful += 1;
+                    let is_updated = String::from_utf8_lossy(&ff_merge.stdout)
+                        .lines()
+                        .filter(|line| line.trim().contains("Already up to date"))
+                        .collect::<Vec<_>>()
+                        .len();
+                    no_update += is_updated;
+                } else {
+                    eprintln!("Skipping branch {branch} due to non-fast-forward merge");
+                    errors.push(GitError::Other(format!(
+                        "Skipping branch {branch} due to non-fast-forward merge"
+                    )));
+                }
+            }
+
+            // If no branches were synced, and there are errors, then we return an error
+            if successful == 0 && !errors.is_empty() {
+                return Err(GitError::Other(
+                    "No branches were successfully synced".to_string(),
+                ));
+            }
+
+            Command::new("git")
+                .args(["-C", tmp_str.as_str(), "push", "origin", "--all"])
+                .status()?;
+
+            Ok((successful, no_update))
+        })
+        .await?;
+        match task {
+            Ok((success, up_to_date)) => {
+                // Essentially, if everything was already up to date, don't report that we've
+                // updated anything
+                if success == up_to_date {
+                    if self.is_tty {
+                        println!(
+                            "All common branches between {url} with {upstream_url} are up to date"
+                        );
+                    }
+                    self.append_slack_message(format!(
+                        "All common branches between {url} and {upstream_url} are up to date"
+                    ))
+                    .await;
+                    return Ok(());
+                }
+                if self.is_tty {
+                    println!("Successfully synced {url} with {upstream_url}");
+                }
+
+                self.append_slack_message(format!("Successfully synced {url} and {upstream_url}"))
+                    .await;
+            }
+            Err(e) => eprintln!("Failed to sync {url} with {upstream_url}: {e}"),
+        }
+        Ok(())
+    }
+    /// Sync all configured repositories. Only repositories that have a parent repository
+    /// should be passed to this function
+    pub async fn sync_all_forks_workaround<T: AsRef<str> + Display>(
+        &self,
+        repositories: HashMap<T, T>,
+    ) -> Result<(), GitError> {
+        let mut futures = FuturesUnordered::new();
+        for (repo, upstream) in repositories {
+            let semaphore_permit = self.semaphore.clone().acquire_owned().await?;
+            futures.push(async move {
+                let result = self
+                    .sync_with_upstream(repo.as_ref(), upstream.as_ref())
+                    .await;
+                drop(semaphore_permit);
+                (repo, upstream, result)
+            });
+        }
+        let mut errors: Vec<(String, GitError)> = Vec::new();
+        while let Some((repo, upstream, result)) = futures.next().await {
+            match result {
+                Ok(()) => {
+                    println!("✅ Successfully synced {repo} with {upstream}");
+                }
+                Err(e) => {
+                    println!("❌ Failed to sync {repo} with {upstream}: {e}");
                     errors.push((repo.to_string(), e));
                 }
             }

commit 1b1d35aa4349cd6e86c177026d377770cb291175

ODP-5234: Add a repository backup blacklist
This adds the ability to blacklist certain repositories for a backup operation
in the git-manage.toml configuration file. If `--blacklist` is passed,
any repository in that list will not be backed up. This is particularly
useful for repositories with few/no changes from upstream that are
large (like Clickhouse).

This also fixes a major performance regression I introduced in
8ad706c969efb166a9d3092124e198647e29db0c where the maximum number
of parallel jobs was always set to 1.

src/cli.rs
@@ -619,6 +619,11 @@ pub struct BackupRepoCommand {
     /// currently.
     #[arg(short, long, default_value_t = false)]
     pub update: bool,
+    /// Enable the backup blacklist to prevent repositories from being backed up from the
+    /// `backup_blacklist` set in git-manage.toml. This option will be ignored if
+    /// `--repository` is specified
+    #[arg(long, default_value_t = false)]
+    pub blacklist: bool,
     /// Bucket name for where you want to store your backup if using `S3`
     #[cfg(feature = "aws")]
     #[arg(short, long, required_if_eq("destination", "s3"))]

src/config.rs
@@ -54,6 +54,7 @@ pub struct Config {
 }
 
 #[derive(Debug, Deserialize, Default)]
+#[allow(clippy::struct_field_names)]
 pub struct MiscSettings {
     /// A blacklist for branches when searching for stale ones.
     /// This allows you to ignore long living branches that should always exist
@@ -61,6 +62,10 @@ pub struct MiscSettings {
     /// A blacklist for licenses. If one of your repositories has one of these licenses, you will
     /// get a warning about it
     pub license_blacklist: Option<HashSet<String>>,
+    /// A blacklist for repositories when you are backing up.
+    /// This allows you to say that you don't want to back up certain repositories, even when
+    /// selecting 'all' as your backup target
+    pub backup_blacklist: Option<HashSet<String>>,
 }
 
 #[derive(Debug, Deserialize, Default)]

src/github/backup.rs
@@ -23,7 +23,8 @@ use crate::utils::compress::compress_directory;
 use crate::utils::repo::http_to_ssh_repo;
 use futures::stream::{FuturesUnordered, StreamExt};
 use indicatif::{ProgressBar, ProgressStyle};
-use std::fmt::Display;
+use std::collections::HashSet;
+use std::fmt::{Debug, Display};
 use std::path::PathBuf;
 use std::process::Command;
 use std::sync::Arc;
@@ -188,10 +189,15 @@ impl GithubClient {
     /// depending on the number of repositories and their sizes
     pub async fn backup_all_repos(
         &self,
-        repositories: &[impl std::fmt::Display + AsRef<str> + ToString],
+        repositories: &[impl std::fmt::Display + AsRef<str> + ToString + Debug],
         path: &Path,
+        blacklist: HashSet<String>,
     ) -> Result<Vec<PathBuf>, GitError> {
         let mut futures = FuturesUnordered::new();
+        let repositories: Vec<_> = repositories
+            .iter()
+            .filter(|r| !blacklist.contains(&r.to_string()))
+            .collect();
         let number_of_repos = repositories.len();
 
         if self.is_tty && number_of_repos > 1 {

src/github/client.rs
@@ -27,7 +27,6 @@ use tokio::sync::Mutex;
 use indexmap::IndexSet;
 use tokio::sync::Semaphore;
 
-use std::cmp;
 use std::collections::HashSet;
 use std::fmt::Write as _;
 use std::io::IsTerminal;
@@ -92,8 +91,6 @@ impl GithubClient {
             .personal_token(github_token.as_ref())
             .build()
             .map_err(GitError::GithubApiError)?;
-        // Shadow max_jobs. This value makes no sense if it's less than 1
-        let max_jobs: usize = cmp::max(1, max_jobs);
         let webhook_url: String = slack_webhook.unwrap_or_default().trim().to_string();
 
         Ok(Self {
@@ -101,6 +98,8 @@ impl GithubClient {
             semaphore: Arc::new(Semaphore::new(max_jobs)),
             webhook_url,
             is_tty: std::io::stdout().is_terminal(),
+            // This is a type that can only be written to once, and represents the type of output
+            // that we have.
             output: Arc::new(OnceCell::new()),
             // Arbitrary initial capacity to avoid allocations if there aren't that many messages
             slack_messages: Arc::new(Mutex::new(Vec::with_capacity(100))),

src/github/match_args.rs
@@ -107,7 +107,7 @@ pub async fn match_arguments(app: &AppArgs, config: Config) -> Result<(), GitErr
         .or_else(|| config.slack.webhook_url.clone());
 
     // This value must be greater than 0
-    let jobs: usize = std::cmp::min(app.jobs.unwrap_or(default_jobs), 1);
+    let jobs: usize = app.jobs.unwrap_or(default_jobs);
 
     let client = GithubClient::new(&token, &config, jobs, slack_webhook)?;
     if !token.is_empty() && verbose {
@@ -570,7 +570,7 @@ async fn match_release_cmds(
 async fn match_backup_cmds(
     client: &GithubClient,
     repos: Vec<String>,
-    _config: Config,
+    config: Config,
     cmd: &BackupCommand,
     fork_workaround: HashMap<String, String>,
 ) -> Result<(), GitError> {
@@ -580,6 +580,14 @@ async fn match_backup_cmds(
             let passed_path = create_cmd.path.as_ref();
             let current_dir;
             let dest = create_cmd.destination;
+            // This is only a meaningful option when '--all' is passed
+            let enable_blacklist = create_cmd.blacklist;
+            let blacklist = if enable_blacklist {
+                config.misc.backup_blacklist.unwrap_or_default()
+            } else {
+                HashSet::new()
+            };
+
             let path = if let Some(p) = passed_path {
                 p
             } else {
@@ -593,7 +601,7 @@ async fn match_backup_cmds(
                 if !fork_workaround.is_empty() {
                     repos.extend(fork_workaround.keys().cloned());
                 }
-                let successful = client.backup_all_repos(&repos[..], path).await?;
+                let successful = client.backup_all_repos(&repos[..], path, blacklist).await?;
                 if dest == BackupDestination::S3 {
                     if let Some(bucket) = bucket {
                         client.backup_all_to_s3(successful, bucket).await?;