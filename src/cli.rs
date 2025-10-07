acceldata-io/git_sync

commit 8ad706c969efb166a9d3092124e198647e29db0c

Added syncing/backups for forks that need to specify their parent repository

src/cli.rs
@@ -217,6 +217,11 @@ pub struct SyncTagCommand {
     /// read/write permissions for the repositories you are syncing.
     #[arg(short, long, default_value_t = false)]
     pub with_annotated: bool,
+
+    /// Sync tags in repositories that are forks, but do not have the parent repository set in a way that
+    /// Github understands.
+    #[arg(long, default_value_t = false, hide = true)]
+    pub with_fork_workaround: bool,
 }
 
 // --- Repo Commands ---
@@ -236,6 +241,7 @@ pub struct SyncTagCommand {
         ArgGroup::new("branch_conflict")
         .args(&["branch", "all"])
 ))]
+#[allow(clippy::struct_excessive_bools)]
 pub struct SyncRepoCommand {
     /// The repository to sync
     #[arg(short, long)]
@@ -253,6 +259,10 @@ pub struct SyncRepoCommand {
     /// Sync a specific branch. Not a valid option when using --all or --recursive
     #[arg(short, long)]
     pub branch: Option<String>,
+    /// Sync branches in repositories that are forks, but do not have the parent repository set in a way that
+    /// Github understands.
+    #[arg(long, default_value_t = false)]
+    pub with_fork_workaround: bool,
 }
 
 /// Check repositories for various conditions

src/github/branch.rs
@@ -200,8 +200,10 @@ impl GithubClient {
         match res {
             Ok(_) => {
                 if !quiet {
-                    self.append_slack_message(format!("New branch {new_branch} created"))
-                        .await;
+                    self.append_slack_message(format!(
+                        "{owner}/{repo} - New branch {new_branch} created"
+                    ))
+                    .await;
                 }
                 Ok(())
             }

src/github/match_args.rs
@@ -79,17 +79,22 @@ pub async fn match_arguments(app: &AppArgs, config: Config) -> Result<(), GitErr
         }
     };
     // Remove any duplicate repositories. This shouldn't have any meaningful performance impact
-    // since there won't every be thousands of repositories configured.
+    // since there won't ever be thousands of repositories configured.
+    // Collect it into a Vec so that we have a consistent ordered collection
     let repos = repos
         .into_iter()
         .collect::<HashSet<_>>()
         .into_iter()
         .collect::<Vec<_>>();
 
+    // If for some reason we cannot get the number of threads and the user doesn't try to define it,
+    // default to 4, which seems like a reasonable minimum expectation for most systems.
     let default_jobs = std::thread::available_parallelism()
         .map(std::num::NonZero::get)
         .unwrap_or(4);
-    let jobs: usize = app.jobs.unwrap_or(default_jobs);
+
+    // This value must be greater than 0
+    let jobs: usize = std::cmp::min(app.jobs.unwrap_or(default_jobs), 1);
     let client = GithubClient::new(&token, &config, jobs)?;
     if !token.is_empty() && verbose {
         let (rest_limit, graphql_limit) =
@@ -103,8 +108,10 @@ pub async fn match_arguments(app: &AppArgs, config: Config) -> Result<(), GitErr
         }
     }
     match &app.command {
-        Command::Tag { cmd } => match_tag_cmds(&client, repos, cmd).await?,
-        Command::Repo { cmd } => match_repo_cmds(&client, repos, config, cmd).await?,
+        Command::Tag { cmd } => match_tag_cmds(&client, repos, cmd, app.repository_type).await?,
+        Command::Repo { cmd } => {
+            match_repo_cmds(&client, repos, config, cmd, app.repository_type).await?
+        }
         Command::Branch { cmd } => match_branch_cmds(&client, repos, cmd, quiet).await?,
         Command::Release { cmd } => match_release_cmds(&client, repos, config, cmd).await?,
         Command::PR { cmd } => match_pr_cmds(&client, repos, config, cmd).await?,
@@ -157,6 +164,7 @@ async fn match_tag_cmds(
     client: &GithubClient,
     repos: Vec<String>,
     cmd: &TagCommand,
+    reposotory_type: RepositoryType,
 ) -> Result<(), GitError> {
     let result = async {
         match cmd {
@@ -279,16 +287,37 @@ async fn match_repo_cmds(
     repos: Vec<String>,
     config: Config,
     cmd: &RepoCommand,
+    repository_type: RepositoryType,
 ) -> Result<(), GitError> {
     match cmd {
         RepoCommand::Sync(sync_cmd) => {
             let repository = sync_cmd.repository.as_ref();
             let recursive = sync_cmd.recursive;
             let branch = sync_cmd.branch.as_ref();
+            let forks_with_workaround = config.repos.fork_workaround.clone().unwrap_or_default();
+            let process_forks_workaround = sync_cmd.with_fork_workaround;
             if sync_cmd.all {
-                client.sync_all_forks(&repos[..], recursive).await?;
+                if repository_type == RepositoryType::Fork && process_forks_workaround {
+                    let (task1, task2) = tokio::join!(
+                        client.sync_all_forks_workaround(forks_with_workaround),
+                        client.sync_all_forks(&repos[..], recursive),
+                    );
+                    task1?;
+                    task2?;
+                } else {
+                    client.sync_all_forks(&repos[..], recursive).await?;
+                }
             } else if let Some(repository) = repository {
-                if recursive {
+                // Process repositories using git
+                if let Some(parent) = forks_with_workaround.get(repository)
+                    && process_forks_workaround
+                {
+                    client.sync_with_upstream(repository, parent).await?;
+                } else if forks_with_workaround.contains_key(repository) {
+                    return Err(GitError::Other(format!(
+                        "Repository '{repository}' has no configured parent. Use the '--with-fork-workaround' flag to enable syncing it."
+                    )));
+                } else if recursive {
                     client.sync_fork_recursive(repository).await?;
                 } else {
                     client.sync_fork(repository, branch).await?;

Cargo.toml
@@ -3,7 +3,7 @@ authors = ["Jeffrey Smith <jeffrey.smith7@gmail.com>"]
 edition = "2024"
 license = "Apache-2.0"
 name    = "git_sync"
-version = "0.3.0"
+version = "0.4.0"
 
 
 [dependencies]

src/config.rs
@@ -216,6 +216,14 @@ impl Config {
         });
         results
     }
+    /// Get all repository urls for the fork workaround group
+    fn get_fork_workaround_repositories(&self) -> Vec<String> {
+        if let Some(fork_workarounds) = &self.repos.fork_workaround {
+            fork_workarounds.keys().cloned().collect()
+        } else {
+            Vec::new()
+        }
+    }
     /// Get all repositories defined in the config file
     pub fn get_all_repositories(&self) -> Vec<String> {
         self.repos
@@ -226,6 +234,7 @@ impl Config {
             .chain(self.get_fork_repositories())
             .chain(self.get_private_repositories())
             .chain(self.get_all_custom_group_repositories())
+            .chain(self.get_fork_workaround_repositories())
             .collect()
     }
 }

commit b3a6bcfec2c9ccff1420f1775011275eecbb7a2c

ODP-5236: Update 'change-version' to be 'modify' (#4)
* ODP-5234: Add a repository backup blacklist

This adds the ability to blacklist certain repositories for a backup operation
in the git-manage.toml configuration file. If `--blacklist` is passed,
any repository in that list will not be backed up. This is particularly
useful for repositories with few/no changes from upstream that are
large (like Clickhouse).

This also fixes a major performance regression I introduced in
8ad706c969efb166a9d3092124e198647e29db0c where the maximum number
of parallel jobs was always set to 1.

* ODP-5236: Update change-version command to 'modify'

This also adds some documentation for the command, as well as cleaning up
options related to it.

* Update README.md

Co-authored-by: Copilot <175728472+Copilot@users.noreply.github.com>

* Update src/github/backup.rs

Remove a trait that was added for debugging purposes.

Co-authored-by: Copilot <175728472+Copilot@users.noreply.github.com>

* ODP-5234: Rename variable and simplify type signature

---------

Co-authored-by: Copilot <175728472+Copilot@users.noreply.github.com>

src/cli.rs
@@ -500,26 +500,33 @@ pub struct DeleteBranchCommand {
     long_about = "
 Examples:
     # Change the version from 3.3.6.2-1 to 3.3.6.2-101
-    git_sync branch change-version --all --branch ODP-3.3.6.2-1 --old-version 3.3.6.2-1 --new-version 3.3.6.2-101
+    git_sync branch change --all --branch ODP-3.3.6.2-1 --old 3.3.6.2-1 --new 3.3.6.2-101
 Notes:
     This action is a regex match and replace, so be careful what you pass as the old version.
     Strictly speaking, this can be used to replace any matching regex within a repository
+
+    If you run this against odp-bigtop in order to update its version, make sure that `is-version` is true.
+    This ensures that it will correctly update filenames and other odp-bigtop specific changes.
 "
 )]
-pub struct ChangeVersionCommand {
-    /// Change the version of a branch in a repository
+pub struct ChangeBranchTextCommand {
+    /// Change text of a branch in a repository
     #[arg(short, long)]
     pub repository: Option<String>,
-    /// Branch to change version in
+    /// Branch to change text in
     #[arg(short, long)]
     pub branch: String,
-    /// The old version you wish to change
+    /// The old text you wish to change
     #[arg(short, long)]
-    pub old_version: String,
-    /// The new version you want to change it to
+    pub old: String,
+    /// The new text you want to change it to
     #[arg(short, long)]
-    pub new_version: String,
-    /// Change the version for the specified branch across all configured repositories
+    pub new: String,
+    /// Specify that the text you're modifying is a version. This affects the default commit
+    /// message as well as running additional steps for odp-bigtop.
+    #[arg(long, default_value_t = false)]
+    pub not_version: bool,
+    /// Change the text for the specified branch across all configured repositories
     #[arg(short, long, default_value_t = false)]
     pub all: bool,
     /// An optional commit message to override the automatic commit message
@@ -713,12 +720,12 @@ pub enum RepoCommand {
 /// Define all the valid commands for acting on branches
 #[derive(Subcommand, Clone, Debug)]
 pub enum BranchCommand {
-    /// Delete a branch in repositories
-    Delete(DeleteBranchCommand),
     /// Create a branch for repositories
     Create(CreateBranchCommand),
-    /// Change version in a branch for repositories
-    ChangeVersion(ChangeVersionCommand),
+    /// Delete a branch in repositories
+    Delete(DeleteBranchCommand),
+    /// Modify text in a branch for repositories
+    Modify(ChangeBranchTextCommand),
 }
 
 /// The top-level command enum for the CLI

src/github/branch.rs
@@ -427,21 +427,22 @@ impl GithubClient {
     /// Clones the specified branch from a repository, updates the release version in all files,
     /// renames package files if necessary, commits, and pushes the changes.
     /// Returns an error if any step fails.
-    #[allow(clippy::too_many_lines)]
-    pub async fn change_release_version(
+    #[allow(clippy::too_many_lines, clippy::too_many_arguments)]
+    pub async fn modify_branch(
         &self,
         url: String,
         branch: String,
-        old_version: String,
-        new_version: String,
+        old_text: String,
+        new_text: String,
         message: Option<String>,
         dry_run: bool,
+        is_version: bool,
     ) -> Result<(), GitError> {
         let info = get_repo_info_from_url(url)?;
         let (repo, owner, url) = (info.repo_name, info.owner, info.url);
         let repo_slack = repo.clone();
-        let old_version_slack = old_version.clone();
-        let new_version_slack = new_version.clone();
+        let old_text_slack = old_text.clone();
+        let new_text_slack = new_text.clone();
         let owner_slack = owner.clone();
         let branch_slack = branch.clone();
 
@@ -467,10 +468,11 @@ impl GithubClient {
             };
 
             let repo_dir = tmp.join(&repo);
-            println!(
-                "Starting to clone {repo}'s branch {branch} into {}",
-                tmp.display()
-            );
+                println!(
+                    "Starting to clone {repo}'s branch {branch} into {}",
+                    tmp.display()
+                );
+
             // Clone the specified branch into a temporary directory
             // We use --depth 1 to only get the latest commit
             // We use --single-branch to only get the specified branch
@@ -496,14 +498,14 @@ impl GithubClient {
             }
 
             let re = REPO_REGEX.get_or_init(|| {
-                let msg = format!("Invalid regex for changing version '{old_version}'");
-                Regex::new(&fancy_regex::escape(old_version.as_ref())).expect(&msg)
+                let msg = format!("Invalid regex for changing version '{old_text}'");
+                Regex::new(&fancy_regex::escape(old_text.as_ref())).expect(&msg)
             });
-            replace_all_in_directory(&repo_dir, re, new_version.as_str());
+            replace_all_in_directory(&repo_dir, re, new_text.as_str());
             // If we're working with the odp-bigtop repo, we also need to rename package files
             // in bigtop-packages/src/deb
-            if repo == "odp-bigtop" {
-                let build_number = new_version
+            if repo == "odp-bigtop" && is_version {
+                let build_number = new_text
                     .split('-')
                     .next_back()
                     .ok_or_else(|| GitError::Other("Invalid new version format".to_string()))?;
@@ -521,14 +523,12 @@ impl GithubClient {
 
                 let dir = format!("{}/bigtop-packages/src/deb", repo_dir.display());
 
-                let old_version_dash = &old_version.to_string().replace('.', "-");
-                let new_version_dash = &new_version.to_string().replace('.', "-");
-                println!("{old_version_dash}");
-                println!("{new_version_dash}");
+                let old_text_dash = &old_text.to_string().replace('.', "-");
+                let new_text_dash = &new_text.to_string().replace('.', "-");
 
                 let bigtop_re = Regex::new(&format!(
                     r"^(.*)-{}([.-].*)(.*)$",
-                    fancy_regex::escape(old_version_dash)
+                    fancy_regex::escape(old_text_dash)
                 ))
                 .expect("Failed to compile old version regex");
 
@@ -540,28 +540,25 @@ impl GithubClient {
                     if path.is_dir() {
                         continue;
                     }
-                    if let Some(file_name) = path.file_name().and_then(|n| n.to_str()) {
-                        println!("File name is {file_name}");
-                        if let Some(caps) = bigtop_re.captures(file_name)?
+                    if let Some(file_name) = path.file_name().and_then(|n| n.to_str()) &&
+                        let Some(caps) = bigtop_re.captures(file_name)?
                         {
-                            println!("Modifying file names...");
                             let prefix = &caps[1];
                             let suffix = &caps[2];
-                            println!("Prefix: {prefix}, Suffix: {suffix}");
-                            let new_file_name = format!("{prefix}-{new_version_dash}{suffix}");
-                            println!("New file name: {new_file_name}");
+                            let new_file_name = format!("{prefix}-{new_text_dash}{suffix}");
                             let new_path = path.with_file_name(new_file_name);
-                            println!("New path: {}", new_path.display());
                             fs::rename(path, &new_path)?;
-                        }
                     }
                 }
             }
             let commit_message = if let Some(msg) = message {
                 msg.to_string()
+            } else if is_version {
+                    format!("[Automated] Changed version from '{old_text}' to '{new_text}'")
             } else {
-                format!("[Automated] Changed version from {old_version} to {new_version}")
+                    format!("[Automated] Changed '{old_text}' to '{new_text}'")
             };
+
             // Commit and push the changes
             Command::new("git")
                 .arg("add")
@@ -585,7 +582,7 @@ impl GithubClient {
                 let stdout = String::from_utf8_lossy(&output.stdout);
                 let stderr = String::from_utf8_lossy(&output.stderr);
 
-                let message = format!("No changes to push in {owner}/{repo} for old version '{old_version}' on branch {branch}");
+                let message = format!("No changes to push in {owner}/{repo} for old version '{old_text}' on branch {branch}");
 
                 if stdout.contains("Everything up-to-date") || stderr.contains("Everything up-to-date") {
                     return Ok(message);
@@ -604,7 +601,7 @@ impl GithubClient {
             Ok(m) => {
                 if m.is_empty() {
                     let message = format!(
-                        "{owner_slack}/{repo_slack}: Successfully changed version from {old_version_slack} to {new_version_slack} for branch {branch_slack}"
+                        "{owner_slack}/{repo_slack}: Successfully changed {old_text_slack} to {new_text_slack} for branch {branch_slack}"
                     );
                     println!("✅ {message}");
                     self.append_slack_message(message).await;
@@ -616,10 +613,10 @@ impl GithubClient {
             }
             Err(e) => {
                 eprintln!(
-                    "❌ Failed to change version from {old_version_slack} to {new_version_slack} for {owner_slack}/{repo_slack}: {e}"
+                    "❌ Failed to change {old_text_slack} to {new_text_slack} for {owner_slack}/{repo_slack}: {e}"
                 );
                 self.append_slack_error(format!(
-                    "{owner_slack}/{repo_slack}: Failed to change version from {old_version_slack} to {new_version_slack}: {e}"
+                    "{owner_slack}/{repo_slack}: Failed to change {old_text_slack} to {new_text_slack}: {e}"
                 )).await;
                 Err(e)
             }
@@ -628,30 +625,33 @@ impl GithubClient {
     /// Change the release version for a specified branch across all configured repositories.
     /// This function clones each repository, updates the version, commits, and pushes the changes.
     /// Errors from each repository are collected and returned as a single error if any occur.
-    pub async fn change_all_release_version(
+    #[allow(clippy::too_many_arguments)]
+    pub async fn modify_all_branches(
         &self,
         branch: String,
-        old_version: String,
-        new_version: String,
+        old_text: String,
+        new_text: String,
         repositories: &[String],
         message: Option<String>,
         dry_run: bool,
+        is_version: bool,
     ) -> Result<(), GitError> {
         let mut futures = FuturesUnordered::new();
         for repo in repositories {
             let branch = branch.to_string();
-            let old_version = old_version.to_string();
-            let new_version = new_version.to_string();
+            let old_text = old_text.to_string();
+            let new_text = new_text.to_string();
             let message = message.clone();
             futures.push(async move {
                 let result = self
-                    .change_release_version(
+                    .modify_branch(
                         repo.to_string(),
                         branch,
-                        old_version,
-                        new_version,
+                        old_text,
+                        new_text,
                         message,
                         dry_run,
+                        is_version,
                     )
                     .await;
                 (repo, result)

src/github/match_args.rs
@@ -301,34 +301,38 @@ async fn match_branch_cmds(
                 client.delete_branch(repository, &delete_cmd.branch).await?;
             }
         }
-        BranchCommand::ChangeVersion(change_version_cmd) => {
-            let repository = change_version_cmd.repository.clone();
-            let message = change_version_cmd.message.clone();
-            let (branch, old_version, new_version) = (
-                change_version_cmd.branch.clone(),
-                change_version_cmd.old_version.clone(),
-                change_version_cmd.new_version.clone(),
+        BranchCommand::Modify(modify_cmd) => {
+            let repository = modify_cmd.repository.clone();
+            let message = modify_cmd.message.clone();
+            // Awkwardly named, I know, but since it's a boolean we don't have too many options
+            let is_version = !modify_cmd.not_version;
+            let (branch, old_text, new_text) = (
+                modify_cmd.branch.clone(),
+                modify_cmd.old.clone(),
+                modify_cmd.new.clone(),
             );
-            if change_version_cmd.all {
+            if modify_cmd.all {
                 client
-                    .change_all_release_version(
+                    .modify_all_branches(
                         branch,
-                        old_version,
-                        new_version,
+                        old_text,
+                        new_text,
                         &repos[..],
                         message,
                         dry_run,
+                        is_version,
                     )
                     .await?;
             } else if let Some(repository) = &repository {
                 client
-                    .change_release_version(
+                    .modify_branch(
                         repository.to_string(),
                         branch,
-                        old_version,
-                        new_version,
+                        old_text,
+                        new_text,
                         message,
                         dry_run,
+                        is_version,
                     )
                     .await?;
             }