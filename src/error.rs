7245c3db7ce9eb6087725ad13f153340f3c7fda6
Jeffrey Smith <jeffrey.smith@acceldata.io>
2026-04-29 16:21:21 -0400

    ODP-5616: Remove unused error type

--- src/error.rs
+++ src/error.rs
@@ -29,12 +29,8 @@ use thiserror::Error;
 pub enum CacheError {
     #[error("IO error: {0}")]
     Io(#[from] std::io::Error),
-    #[error("Failed to deserialize cache: {0}")]
-    Deserialize(#[from] serde_json::Error),
-    // You need to map this error for it to work.
-    // Ex: serde_json::to_vec_pretty(&*inner).map_err(CacheError::Serialize)?;
-    #[error("Failed to serialize cache: {0}")]
-    Serialize(serde_json::Error),
+    #[error("Failed to serialize/deserialize Json for cache: {0}")]
+    JsonError(#[from] serde_json::Error),
     #[error("Cache entry invalid for {key}: {reason}")]
     Invalid { key: String, reason: String },
     #[error("Lock Error: {0}")]
